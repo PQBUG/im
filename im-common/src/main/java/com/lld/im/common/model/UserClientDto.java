52dfe64d52a6b23a8ee3b68fc0717c12625286d8
PQBUG <hongyujia404@gmail.com>
2024-02-04 15:45:46 +0800

    created

---
+++ im-common/src/main/java/com/lld/im/common/model/UserClientDto.java
@@ -0,0 +1,17 @@
+package com.lld.im.common.model;
+
+import lombok.Data;
+
+
+@Data
+public class UserClientDto {
+
+    private Integer appId;
+
+    private Integer clientType;
+
+    private String userId;
+
+    private String imei;
+
+}
